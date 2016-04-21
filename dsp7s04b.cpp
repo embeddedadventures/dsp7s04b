c9aaa8e54a795d2872dc911eba422eacc97fabc3
dr4nc3s <david.rances070@gmail.com>
2016-04-21 15:01:05 -0400

    Update dsp7s04b.cpp

--- dsp7s04b.cpp
+++ dsp7s04b.cpp
@@ -1,6 +1,6 @@
 /*
 
-Copyright (c) 2015, Embedded Adventures, www.embeddedadventures.com
+Copyright (c) 2016, Embedded Adventures, www.embeddedadventures.com
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
